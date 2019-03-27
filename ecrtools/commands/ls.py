1cc0f12726e8043e2bf9e38b3f2f5073eeca1f82
Borislav Ivanov <borogl@gmail.com>
2019-03-26 23:01:58 -0700

    basic repo ls

--- ecrtools/commands/ls.py
+++ ecrtools/commands/ls.py
@@ -2,7 +2,27 @@ import click
 
 
 @click.command()
+@click.argument('names', required=False, nargs=-1)
 @click.pass_context
-def ls(ctx):
+def ls(ctx, names):
     '''List repos'''
-    print('this is the ls command')
+    params = {}
+    if len(names):
+        params['repositoryNames'] = [name for name in names]
+
+    repos = describe_repositories(ctx, params)
+    print('\n'.join(sorted([r['repositoryName'] for r in repos])))
+
+
+def describe_repositories(ctx, params={}):
+    response = None
+    repos = []
+    while True:
+        if response:
+            if 'NextMarker' not in response:
+                break
+            else:
+                params['nextToken'] = response['NextMarker']
+        response = ctx.obj['ecr'].describe_repositories(**params)
+        repos += response['repositories']
+    return repos
