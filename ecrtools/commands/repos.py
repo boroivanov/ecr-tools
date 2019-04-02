c1b987b5db2e713033795234e050903b9f3edda6
Borislav Ivanov <borogl@gmail.com>
2019-04-01 18:12:04 -0700

    add option to print repo stats

--- ecrtools/commands/repos.py
+++ ecrtools/commands/repos.py
@@ -1,12 +1,17 @@
+import sys
 import click
+import threading
+import queue
 
 from ecrtools.lib.ecr import Ecr
+from ecrtools.lib.utils import convert_bytes
 
 
 @click.command()
 @click.argument('names', required=False, nargs=-1)
+@click.option('-a', '--all_stats', is_flag=True, help='Print stats.')
 @click.pass_context
-def repos(ctx, names):
+def repos(ctx, names, all_stats):
     '''List repos'''
 
     ecr = Ecr(ctx.obj['ecr'])
@@ -15,4 +20,51 @@ def repos(ctx, names):
         params['repositoryNames'] = [name for name in names]
 
     repos = ecr.describe_repositories(params)
-    click.echo('\n'.join(sorted([r['repositoryName'] for r in repos])))
+
+    if not all_stats:
+        click.echo('\n'.join(sorted([r['repositoryName'] for r in repos])))
+        sys.exit(0)
+
+    print_repo_stats(ctx, repos)
+
+
+def print_repo_stats(ctx, repos):
+    stats = bulk_repo_stats(ctx, repos)
+
+    repo_name_pad = len(max([r['repositoryName'] for r in stats], key=len))
+    for repo in stats:
+        total_size = 0
+        total_untagged = 0
+        click.echo(f"{repo['repositoryName']:{repo_name_pad}}", nl=False)
+        for image in repo['stats']:
+            if 'imageTags' not in image:
+                total_untagged += 1
+            total_size += image['imageSizeInBytes']
+
+        total_size = convert_bytes(total_size, 'GB')
+        click.echo(f'  images: {len(repo)}'
+                   f' untagged: {total_untagged}'
+                   f' total size: {total_size["value"]:.1f}'
+                   f'{total_size["units"]}')
+
+
+def bulk_repo_stats(ctx, repos):
+    q = queue.Queue()
+    threads = []
+    for repo in repos:
+        t = threading.Thread(target=get_repo_stats, args=(ctx, repo, q))
+        t.start()
+        threads.append(t)
+
+    [t.join() for t in threads]
+
+    return [q.get(t) for t in threads]
+
+
+def get_repo_stats(ctx, repo, q):
+    ecr = Ecr(ctx.obj['ecr'], repo['repositoryName'])
+    image_ids = ecr.get_image_ids(image='', exact_match=False)
+    q.put({
+        'repositoryName': repo['repositoryName'],
+        'stats': ecr.get_images(image_ids)}
+    )
